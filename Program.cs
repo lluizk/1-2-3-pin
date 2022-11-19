2e1a673a2ac1c59bf6d377c8cec467f25096e530
Sergio luiz <segioj995@hotmail.com>
2022-11-18 21:25:44 -0300

    adiciona a solução ao exercicio

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿int n = 1;
+
+Console.ForegroundColor= ConsoleColor.DarkRed;
+System.Console.WriteLine("----UmDoisTresPim----");
+System.Console.WriteLine();
+Console.ResetColor();
+
+for (n = 1; n <= 40; n++)
+
+if (n % 4 == 0)
+{
+    Console.ForegroundColor= ConsoleColor.Green;
+    System.Console.Write(" pim\n");
+    Console.ResetColor();
+}
+
+else
+{
+    Console.ForegroundColor= ConsoleColor.White;
+    System.Console.Write($"{n,2} ");
+    Console.ResetColor();
+}
